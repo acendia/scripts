a7f2f9a2dccbb629bd1c8ad11af268afd324caba
Evangelos Kostakis <evangeloskostakis@icarus.local>
2023-07-24 10:29:22 +0300

    added pdf reader script

--- pdf-reader/script_pdf_reader.py
+++ pdf-reader/script_pdf_reader.py
@@ -1,20 +1,20 @@
+# Import the PyPDF2 library
 import PyPDF2
 
 # Open the PDF file
 pdf_file = open('example.pdf', 'rb')
 
 # Create a PdfReader object
-pdf_reader = PyPDF2.PdfFileReader(pdf_file)
+pdf_reader = PyPDF2.PdfReader(pdf_file)
 
 # Get the number of pages
-num_pages = pdf_reader.getNumPages()
+num_pages = len(pdf_reader.pages)
 
 # Loop through the pages and extract text
 for page_num in range(num_pages):
-    page = pdf_reader.getPage(page_num)
-    text = page.extractText()
+    page = pdf_reader.pages[page_num]
+    text = page.extract_text()
     print(text)
 
 # Close the PDF file
 pdf_file.close()
-
